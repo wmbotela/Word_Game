37b46f052c6e4d050978bc245ab30556d178f06a
wmbotela <wmbotela@gmail.com>
2017-04-25 05:51:35 +0300

    App.java
    Adds the up to java file

--- hangman/src/main/java/Hangman.java
+++ hangman/src/main/java/Hangman.java
@@ -1,3 +1,3 @@
-public class Hangman{
+public class Hangman {
   
 }
--- hangman/src/test/java/HangmanTest.java
+++ hangman/src/test/java/HangmanTest.java
@@ -2,5 +2,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HangmanTest {
+  @Test
+  public void newHangman_createsAnewInstance_true(){
+    Hangman testHangman = new Hangman();
+    assertEquals(true, testHangman instanceof Hangman);
+  }
 
 }
